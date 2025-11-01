bb5657ee832efc9765077aeeec5cf0732a2ad98f
Hyper Engineer <admin-user@hyper-engineer.com>
2025-10-31 21:09:36 -0500

    Adding Reflection Workflow changes

---
+++ reflection-agent/src/main/java/engineer/hyper/agentic/controller/ApplicationController.java
@@ -0,0 +1,21 @@
+package engineer.hyper.agentic.controller;
+
+import engineer.hyper.agentic.agent.ReflectionAgent;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    @Autowired
+    private ReflectionAgent reflectionAgent;
+
+    @GetMapping("/ping")
+    public String ping(@RequestParam(defaultValue = "Generate a Java implementation of the Merge Sort algorithm") String question) {
+        return reflectionAgent.run(question, 2);
+    }
+
+}
